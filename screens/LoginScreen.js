259e21b743dfdc7c7ec599f03156fce5bb72d6b7
Omnivers <Twersimor.stock@outlook.com>
2023-02-01 00:59:10 +0100

    Add login inputs without style

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,12 +1,36 @@
 import React from 'react';
-import { StyleSheet,Text,View } from 'react-native';
+import { KeyboardAvoidingView, StyleSheet,Text,TextInput,View } from 'react-native';
 
 const LoginScreen=()=>{
+    const styles= StyleSheet.create({
+        container:{
+            flex:1,
+            justifyContent:'center',
+            alignItems:'center',
+        }
+    })
     return(
-        <View>
-            <Text>Login Screen</Text>
-        </View>
+        <KeyboardAvoidingView
+            style={styles.container}
+            behavior="padding"
+        >
+            <View style={styles.inputContainer}>
+                <TextInput 
+                    placeholder='Email'
+                    // value={ }
+                    // onChangeText={text => }
+                    style={styles.input}
+                />
+                <TextInput 
+                    placeholder='Password'
+                    // value={ }
+                    // onChangeText={text => }
+                    style={styles.input}
+                    secureTextEntry
+                />
+            </View>
+        </KeyboardAvoidingView>
+        
     )
 }
 export default LoginScreen;
-const styles= StyleSheet.create({})
